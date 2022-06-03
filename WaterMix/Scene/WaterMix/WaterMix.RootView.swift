a2483655e2dc1748c53413f3390431cf8276a69e
jmkim <wjdals0304@gmail.com>
2022-06-03 16:06:46 +0900

    feat: WaterMix.Root view 추가

--- WaterMix/Scene/WaterMix/WaterMix.RootView.swift
+++ WaterMix/Scene/WaterMix/WaterMix.RootView.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+import SnapKit
+
+extension WaterMix {
+    
+    final class RootView: BaseUIView {
+        
+        private let totalAccountView = TotalAccountView()
+                
+        override func setup() {
+            
+            [
+                totalAccountView
+            ].forEach { self.addSubview($0) }
+            
+        }
+        
+        override func setupConstraints() {
+            totalAccountView.snp.makeConstraints { make in
+                make.top.equalTo(self.safeAreaLayoutGuide).offset(18)
+                make.leading.equalToSuperview().offset(15)
+                make.width.equalTo(self).multipliedBy(0.92)
+                make.height.equalTo(157)
+            }
+        }
+        
+    }
+}
