82693c3da7f0817d5f812f62af5accf599beaa52
dexlab-tony <tony@dexlab.space>
2022-09-04 19:43:58 +0900

    feat: int extension

---
+++ WaterMix/Extension/Int+.swift
@@ -0,0 +1,24 @@
+//
+//  Int+.swift
+//  WaterMix
+//
+//  Created by tony on 2022/09/04.
+//
+
+import Foundation
+
+extension Int {
+    func withCommas() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value:self))!
+    }
+}
+
+extension Int64 {
+    func withCommas() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value:self))!
+    }
+}
