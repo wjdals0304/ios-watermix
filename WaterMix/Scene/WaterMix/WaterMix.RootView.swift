97a2341f96a3925f59eee8a511d3ba3478f2c14d
jmkim <wjdals0304@gmail.com>
2022-09-04 11:50:16 +0900

    feat: 계산 함수 추가

--- WaterMix/Scene/WaterMix/WaterMix.RootView.swift
+++ WaterMix/Scene/WaterMix/WaterMix.RootView.swift
@@ -4,21 +4,41 @@
 //
 //  Created by 김정민 on 2022/06/03.
 //
-
-import Foundation
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
+import Foundation
 
 extension WaterMix {
     
     final class RootView: BaseUIView {
         
+        let disposeBag = DisposeBag()
+        private let viewModel: WaterMixModelType
+        
         private let totalAccountView = TotalAccountView()
+        
+        private let currentStockView = StockCurrentView(title: L10n.currentStock.description, stockMarketPrice: L10n.currentMarketPrice.description, stockPriceText: L10n.currentStockPrice.description, sotckAmountText: L10n.currentStockAmount.description, totalPriceText: L10n.currentStockTotalPrice.description)
+        
+        
+        private let stockAddView = StockAddView(title: L10n.addStock.description, stockPriceText: L10n.addStockPrice.description, sotckAmountText: L10n.addStockAmount.description, totalPriceText: L10n.addStockTotalPrice.description)
                 
+        init(viewModel: WaterMixModelType) {
+            self.viewModel = viewModel
+            super.init(frame: .zero)
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
         override func setup() {
             
             [
-                totalAccountView
+                totalAccountView,
+                currentStockView,
+                stockAddView
             ].forEach { self.addSubview($0) }
             
         }
@@ -28,9 +48,95 @@ extension WaterMix {
                 make.top.equalTo(self.safeAreaLayoutGuide).offset(18)
                 make.leading.equalToSuperview().offset(15)
                 make.width.equalTo(self).multipliedBy(0.92)
-                make.height.equalTo(157)
+                make.height.equalTo(200)
+            }
+            
+            currentStockView.snp.makeConstraints { make in
+                make.top.equalTo(totalAccountView.snp.bottom).offset(33)
+                make.leading.equalToSuperview().offset(15)
+                make.width.equalTo(self).multipliedBy(0.92)
+                make.height.equalTo(250)
+            }
+            
+            stockAddView.snp.makeConstraints { make in
+                make.top.equalTo(currentStockView.snp.bottom).offset(30)
+                make.leading.equalToSuperview().offset(15)
+                make.width.equalTo(self).multipliedBy(0.92)
+                make.height.equalTo(250)
+                
             }
         }
         
+        override func bind() {
+            
+            currentStockView.stockMarketPriceField.rx.text.orEmpty
+                .map { UInt64($0) ?? 0 }
+                .bind(to: viewModel.getCurrentMarketPrice)
+                .disposed(by: disposeBag)
+            
+            currentStockView.stockPriceTextField.rx.text.orEmpty
+                .map { UInt64($0) ?? 0 }
+                .bind(to: viewModel.getCurrentStockPrice)
+                .disposed(by: disposeBag)
+            
+            currentStockView.stockAmountTextField.rx.text.orEmpty
+                .map { UInt64($0) ?? 0 }
+                .bind(to: viewModel.getcurrentStockAmount)
+                .disposed(by: disposeBag)
+                        
+            viewModel.getCalCurrentTotatlPrice
+                .map { String($0) }
+                .distinctUntilChanged()
+                .bind(to: currentStockView.totalPriceValue.rx.text)
+                .disposed(by: disposeBag)
+            
+            
+            stockAddView.stockPriceTextField.rx.text.orEmpty
+                .map { UInt64($0) ?? 0 }
+                .bind(to: viewModel.getAddStockPrice)
+                .disposed(by: disposeBag)
+            
+            stockAddView.stockAmountTextField.rx.text.orEmpty
+                .map { UInt64($0) ?? 0 }
+                .bind(to: viewModel.getAddStockAmount)
+                .disposed(by: disposeBag)
+            
+            
+            viewModel.getAddTotalPrice
+                .map { String($0) }
+                .distinctUntilChanged()
+                .bind(to: stockAddView.totalPriceValue.rx.text)
+                .disposed(by: disposeBag)
+            
+            viewModel.getTotalPrice
+                .map { String($0) }
+                .distinctUntilChanged()
+                .bind(to: totalAccountView.totalPriceValue.rx.text)
+                .disposed(by: disposeBag)
+            
+            viewModel.getTotalAmount
+                .map { String($0) }
+                .distinctUntilChanged()
+                .bind(to: totalAccountView.totalAmountValue.rx.text)
+                .disposed(by: disposeBag)
+            
+            viewModel.getTotalPurchasePrice
+                .map { String(Int64($0)) }
+                .distinctUntilChanged()
+                .bind(to: totalAccountView.totalStockValue.rx.text )
+                .disposed(by: disposeBag)
+            
+            viewModel.getTotalRatio
+                .map { String($0) + "%" }
+                .distinctUntilChanged()
+                .bind(to: totalAccountView.proAndLossRatioValue.rx.text)
+                .disposed(by: disposeBag)
+            
+            viewModel.getProAndLoss
+                .map { String($0) }
+                .distinctUntilChanged()
+                .bind(to: totalAccountView.proAndLossValue.rx.text)
+                .disposed(by: disposeBag)
+        }
     }
 }
