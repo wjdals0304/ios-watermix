ff68bef68bd88e0d0b5809991e0ed1f92b837221
jmkim <wjdals0304@gmail.com>
2023-04-09 20:21:13 +0900

    feat: 앱 다국어 처리

--- WaterMix/Resources/Strings+Generated.swift
+++ WaterMix/Resources/Strings+Generated.swift
@@ -10,36 +10,42 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum L10n {
-  /// 추가 매수
-  internal static var addStock: String { L10n.tr("Localizable", "AddStock") }
-  /// 추매 수량
-  internal static var addStockAmount: String { L10n.tr("Localizable", "AddStockAmount") }
-  /// 추매 평단가
-  internal static var addStockPrice: String { L10n.tr("Localizable", "AddStockPrice") }
-  /// 총 추매 금액
-  internal static var addStockTotalPrice: String { L10n.tr("Localizable", "AddStockTotalPrice") }
-  /// 현재 시장가
-  internal static var currentMarketPrice: String { L10n.tr("Localizable", "CurrentMarketPrice") }
-  /// 현재 보유 주식
-  internal static var currentStock: String { L10n.tr("Localizable", "CurrentStock") }
-  /// 보유 수량
-  internal static var currentStockAmount: String { L10n.tr("Localizable", "CurrentStockAmount") }
-  /// 보유 주식 평단가
-  internal static var currentStockPrice: String { L10n.tr("Localizable", "CurrentStockPrice") }
-  /// 총 매입 금액
-  internal static var currentStockTotalPrice: String { L10n.tr("Localizable", "CurrentStockTotalPrice") }
-  /// 손익금
-  internal static var proAndLoss: String { L10n.tr("Localizable", "proAndLoss") }
-  /// 손익률
-  internal static var ratio: String { L10n.tr("Localizable", "Ratio") }
-  /// 평단가 계산기
-  internal static var title: String { L10n.tr("Localizable", "Title") }
-  /// 최종 수량
-  internal static var totalAmount: String { L10n.tr("Localizable", "TotalAmount") }
-  /// 최종 매수 금액
-  internal static var totalPrice: String { L10n.tr("Localizable", "TotalPrice") }
-  /// 최종 평단가
-  internal static var totalPurchasePrice: String { L10n.tr("Localizable", "TotalPurchasePrice") }
+  internal enum InfoPlist {
+    /// 워터독(평단가계산기)
+    internal static var cfBundleDisplayName: String { L10n.tr("InfoPlist", "CFBundleDisplayName") }
+  }
+  internal enum Localizable {
+    /// 추가 매수
+    internal static var addStock: String { L10n.tr("Localizable", "AddStock") }
+    /// 추매 수량
+    internal static var addStockAmount: String { L10n.tr("Localizable", "AddStockAmount") }
+    /// 추매 평단가
+    internal static var addStockPrice: String { L10n.tr("Localizable", "AddStockPrice") }
+    /// 총 추매 금액
+    internal static var addStockTotalPrice: String { L10n.tr("Localizable", "AddStockTotalPrice") }
+    /// 현재 시장가
+    internal static var currentMarketPrice: String { L10n.tr("Localizable", "CurrentMarketPrice") }
+    /// 현재 보유 주식
+    internal static var currentStock: String { L10n.tr("Localizable", "CurrentStock") }
+    /// 보유 수량
+    internal static var currentStockAmount: String { L10n.tr("Localizable", "CurrentStockAmount") }
+    /// 보유 주식 평단가
+    internal static var currentStockPrice: String { L10n.tr("Localizable", "CurrentStockPrice") }
+    /// 총 매입 금액
+    internal static var currentStockTotalPrice: String { L10n.tr("Localizable", "CurrentStockTotalPrice") }
+    /// 손익금
+    internal static var proAndLoss: String { L10n.tr("Localizable", "proAndLoss") }
+    /// 손익률
+    internal static var ratio: String { L10n.tr("Localizable", "Ratio") }
+    /// 평단가 계산기
+    internal static var title: String { L10n.tr("Localizable", "Title") }
+    /// 최종 수량
+    internal static var totalAmount: String { L10n.tr("Localizable", "TotalAmount") }
+    /// 최종 매수 금액
+    internal static var totalPrice: String { L10n.tr("Localizable", "TotalPrice") }
+    /// 최종 평단가
+    internal static var totalPurchasePrice: String { L10n.tr("Localizable", "TotalPurchasePrice") }
+  }
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
